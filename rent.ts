81e1fb5374f8220dc0fbf4761b0461789b74581f
RennamFariaUNIFESP <143340352+RennamFariaUNIFESP@users.noreply.github.com>
2023-09-11 21:07:13 -0300

    Update rent.ts

--- rent.ts
+++ rent.ts
@@ -2,24 +2,24 @@ import { Bike } from "./bike";
 import { User } from "./user";
 
 export class Rent {
+    dateEnd: Date = undefined
+    
     private constructor(
         public bike: Bike,
         public user: User,
-        public dateFrom: Date,
-        public dateTo: Date,
-        public dateReturned?: Date
+        public startDate: Date,
     ) {}
 
     static create(rents: Rent[], bike: Bike, user: User, 
-                  startDate: Date, endDate: Date): Rent {
-        const canCreate = Rent.canRent(rents, startDate, endDate)
-        if (canCreate) return new Rent(bike, user, startDate, endDate)
+                  dateNow: Date): Rent {
+        const canCreate = Rent.canRent(rents, dateNow)
+        if (canCreate) return new Rent(bike, user, dateNow)
         throw new Error('Overlapping dates.')
     }
 
-    static canRent(rents: Rent[], startDate: Date, endDate: Date): boolean {
+    static canRent(rents: Rent[], dateNow: Date): boolean {
         for (const rent of rents) {
-            if (startDate <= rent.dateTo && endDate >= rent.dateFrom) {
+            if (startDate <= rent.dateTo) {
                 return false
             }
         }
